4899c72d4af8499becfb6e4e71d9d456c1fb7ee2
lee dong kyoo <dk910320@gmail.com>
2014-11-08 17:10:35 +0900

    adf

--- AjouStartup/src/dk/app/AjouStartup/SplashActivity.java
+++ AjouStartup/src/dk/app/AjouStartup/SplashActivity.java
@@ -1,18 +1,29 @@
 package dk.app.AjouStartup;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
 
 public class SplashActivity extends Activity{
 
 	
-	
+	private final int SPLASH_DISPLAY_LENGTH = 1000;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		
-		
+		new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashActivity.this,MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
 		
 		
 	}
